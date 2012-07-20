2fbe83360d7772880e4303e799039fe47fc64437
Rafael Henrique Moreira <rafaelmoreira88@gmail.com>
2012-07-20 19:00:26 -0300

    New user.py handler do not implemented yet

---
+++ myapp/views/user.py
@@ -0,0 +1,106 @@
+# myapp.views.user
+#
+# Handler: LoginHandler (POST)
+# It will login users, and exibe user pages
+
+import webapp2
+import jinja2
+import os
+
+from aux import is_development
+
+
+from ..models.player import Player
+
+jinja_environment = jinja2.Environment(
+  loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + '/../templates/'))
+
+
+class LoginHandler(webapp2.RequestHandler):
+  def post(self, game_id):
+    
+    nickname = self.request.get('nickname')
+    password = self.request.get('password')
+    
+    query = Player.all()
+    query.filter('nickname =', nickname)
+    player = query.get()
+    
+    
+    player = get_current_player()
+    option = self.request.get('option')
+    
+    if not player.is_anonymous:
+      query = Game.all()
+      query.filter('active =', True)
+      query.filter('game_id =', game_id)
+      game = query.get()
+      if game:
+        query = Status.all()
+        query.filter('player =', player)
+        query.filter('game =', game)
+        status = query.get()
+        
+        if option == 'sign_up':
+          if not status:
+            status = Status(player=player, game=game)
+            status.put()
+            game.online_players += 1
+            game.put()
+        
+        elif option == 'sign_in':
+          if status:
+            if status.playing == False:
+              status.playing = True
+              status.put()
+              game.online_players += 1
+              game.put()
+        
+        elif option == 'sign_out':
+          if status:
+            if status.playing == True:
+              status.playing = False
+              status.put()
+              game.online_players -= 1
+              game.put()
+    
+    self.redirect(self.request.uri)
+  
+  def get(self, game_id):
+    player = get_current_player()
+    
+    
+    query = Game.all()
+    query.filter('active =', True)
+    query.filter('game_id =', game_id)
+    game = query.get()
+    
+    status = None
+    if not player.is_anonymous:
+      query = Status.all()
+      query.filter('player =', player)
+      query.filter('game =', game)
+      status = query.get()
+    
+    query = Status.all()
+    query.filter('game =', game)
+    query.filter('playing =', True)
+    status_list = query.fetch(limit=None)
+    
+    query = Match.all()
+    query.filter('game =', game)
+    query.filter('finished =', False)
+    matchs = query.fetch(limit=None)
+    
+    template_values = {
+      'is_development': is_development,
+      'player': player,
+      'game': game,
+      'status': status,
+      'status_list': status_list,
+      'matchs': matchs,
+      }
+    template = jinja_environment.get_template('game.html')
+    self.response.out.write(template.render(template_values))
+
+
